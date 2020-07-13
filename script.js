0c1ec4e561231ba4cbb63896814ad078aaee4d47
davidat <davidatencia2000@gmail.com>
2020-07-13 02:00:08 -0500

    con responsive (css precario pero funciona)

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 'use strict'
 
-var boton = document.querySelector("#compartir");
+var boton = document.querySelector(".compartir");
 var facebook = document.querySelector("#facebook");
 var twitter = document.querySelector("#twitter");
 var pinterest = document.querySelector("#pinterest");
 
 boton.addEventListener("click", function(){
-    var ventana = document.querySelector("#shared");
+    var ventana = document.querySelector(".shared");
+    var w = window.innerWidth
+            || document.documentElement.clientWidth
+            || document.body.clientWidth;
     if(ventana.style.display == "none"){
         ventana.style.display = "flex";
+        if(w="730"){
+            var ven= document.querySelector(".contacto");
+            ven.style.display= "none";
+        }
     }else{
         ventana.style.display = "none";
     }
